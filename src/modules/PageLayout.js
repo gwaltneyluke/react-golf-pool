78d518379005e62a062699215c05902db267768d
Luke Gwaltney <lgwaltney@zoll.com>
2022-05-09 16:04:52 -0400

    feat: add makepicks button

--- src/modules/PageLayout.js
+++ src/modules/PageLayout.js
@@ -1,6 +1,19 @@
+import { useState } from 'react';
 import GentLeaderboard from './GentLeaderboard';
 
+const defaultUser = {
+    signedIn: false
+};
+
 function PageLayout() {
+    const [ user, setUser ] = useState(defaultUser)
+    
+    const handleLoginClick = () => {
+        setUser({
+            signedIn: true
+        })
+    }
+
     return (
         <div class='App'>
             <table class='title-and-login-bar'>
@@ -11,7 +24,10 @@ function PageLayout() {
                             <h1 class='title'>Hello, friends</h1>
                         </td>
                         <td>
-                            <button class='login-button'>Login</button>
+                            {user.signedIn ? 
+                                (<button class='make-picks-button'>Make Picks</button>) :
+                                (<button class='login-button' onClick={handleLoginClick}>Login</button>)
+                            }
                         </td>
                     </tr>
                 </tbody>
